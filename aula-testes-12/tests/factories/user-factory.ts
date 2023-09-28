dde335ea79a98dd450f19c02d417ba21c54ccbc0
gpsign <gpsignorini@gmail.com>
2023-09-28 10:08:03 -0300

    feat: aula 12

---
+++ aula-testes-12/tests/factories/user-factory.ts
@@ -0,0 +1,25 @@
+import prisma from "../../src/database";
+import { UserInput } from "../../src/repository";
+import { faker } from "@faker-js/faker";
+
+export async function buildUser(email: string, password?: string) {
+	const userData: UserInput = {
+		email,
+		password: password || new Date().getTime().toString(),
+	};
+
+	const user = await prisma.user.create({ data: userData });
+	return user;
+}
+
+export async function buildRandomUser() {
+	const { email, password } = randomUser();
+	return buildUser(email, password);
+}
+
+export function randomUser() {
+	return {
+		email: faker.internet.email(),
+		password: faker.internet.password(),
+	};
+}
